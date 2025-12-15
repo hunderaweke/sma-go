cfd642597a0070f8f6329b11ca19d386db3bf67d
Hundera Awoke <hunderaweke@gmail.com>
2025-12-15 15:25:09 +0300

    update: reorder fields in Message struct for consistency

--- domain/message.go
+++ domain/message.go
@@ -4,11 +4,9 @@ import "gorm.io/gorm"
 
 type Message struct {
 	gorm.Model
-	FromUnique string `gorm:"index;not null"` // FK to Indentity.UniqueString
-	ToUnique   string `gorm:"index;not null"` // FK to Indentity.UniqueString
-
-	From Identity `gorm:"foreignKey:FromUnique;references:UniqueString;constraint:OnUpdate:CASCADE,OnDelete:SET NULL"`
-	To   Identity `gorm:"foreignKey:ToUnique;references:UniqueString;constraint:OnUpdate:CASCADE,OnDelete:SET NULL"`
-
-	Text string `gorm:"type:text;not null"`
+	FromUnique string   `gorm:"index;not null"`
+	ToUnique   string   `gorm:"index;not null"`
+	From       Identity `gorm:"foreignKey:FromUnique;references:UniqueString;constraint:OnUpdate:CASCADE,OnDelete:SET NULL" json:"-"`
+	To         Identity `gorm:"foreignKey:ToUnique;references:UniqueString;constraint:OnUpdate:CASCADE,OnDelete:SET NULL" json:"-"`
+	Text       string   `gorm:"type:text;not null"`
 }
