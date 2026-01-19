4050b95c17aafcea7b8e77d920de9a720afdac23
Hundera Awoke <hunderaweke@gmail.com>
2026-01-19 21:06:25 +0300

    feat: add sse for realtime message receiving

--- main.go
+++ main.go
@@ -1,12 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
-	"github.com/gofiber/contrib/socketio"
-	"github.com/gofiber/contrib/websocket"
-	"github.com/gofiber/fiber/v2"
 	_ "github.com/hunderaweke/sma-go/config"
 	"github.com/hunderaweke/sma-go/database"
 	"github.com/hunderaweke/sma-go/domain"
@@ -37,22 +33,6 @@ func main() {
 	analyticsUC := usecases.NewAnalyticsUsecase(analyticsRepo)
 
 	app := router.NewRouter(identityUC, messageUC, analyticsUC)
-	app.Use(func(c *fiber.Ctx) error {
-		if websocket.IsWebSocketUpgrade(c) {
-			c.Locals("allowed", true)
-			return c.Next()
-		}
-		return fiber.ErrUpgradeRequired
-	})
-	socketio.On(socketio.EventConnect, func(ep *socketio.EventPayload) {
-		fmt.Printf("Connection event 1 - Payload: %+v", ep)
-	})
-	app.Get("/ws/:id", socketio.New(func(kws *socketio.Websocket) {
-		uniqueString := kws.Params("unique")
-		kws.SetAttribute("unique_string", uniqueString)
-		kws.Broadcast([]byte(fmt.Sprintf("New user connected: %s and UUID: %s", uniqueString, kws.UUID)), true, socketio.TextMessage)
-		kws.Emit([]byte(fmt.Sprintf("Hello user: %s with UUID: %s", uniqueString, kws.UUID)), socketio.TextMessage)
-	}))
 	if err := app.Listen(":3000"); err != nil {
 		log.Fatalf("server stopped: %v", err)
 	}
--- server/controller/message.go
+++ server/controller/message.go
@@ -1,17 +1,47 @@
 package controller
 
 import (
+	"bufio"
+	"fmt"
+	"log"
+	"sync"
+	"time"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/hunderaweke/sma-go/domain"
 	"github.com/hunderaweke/sma-go/options"
+	"github.com/valyala/fasthttp"
 )
 
 type MessageController struct {
-	usecase domain.MessageUsecase
+	messageChannels sync.Map
+	usecase         domain.MessageUsecase
 }
 
 func NewMessageController(uc domain.MessageUsecase) *MessageController {
-	return &MessageController{usecase: uc}
+	return &MessageController{usecase: uc, messageChannels: sync.Map{}}
+}
+
+func (mc *MessageController) addClient(uniqueString string) chan string {
+	ch := make(chan string)
+	mc.messageChannels.Store(uniqueString, ch)
+	return ch
+}
+
+func (mc *MessageController) removeClient(uniqueString string) {
+	mc.messageChannels.Delete(uniqueString)
+}
+
+func (mc *MessageController) sendToClient(uniqueString, msg string) {
+	value, ok := mc.messageChannels.Load(uniqueString)
+	if !ok {
+		return
+	}
+	ch := value.(chan string)
+	select {
+	case ch <- msg:
+	default:
+	}
 }
 
 func (mc *MessageController) Create(c *fiber.Ctx) error {
@@ -27,6 +57,7 @@ func (mc *MessageController) Create(c *fiber.Ctx) error {
 	if err != nil {
 		return writeDomainError(c, err)
 	}
+	mc.sendToClient(req.ToUnique, msg.Text)
 	return c.Status(fiber.StatusCreated).JSON(msg)
 }
 
@@ -67,4 +98,40 @@ func (mc *MessageController) Delete(c *fiber.Ctx) error {
 		return writeDomainError(c, err)
 	}
 	return c.SendStatus(fiber.StatusNoContent)
+}
+
+func (mc *MessageController) ReceiveMessages(c *fiber.Ctx) error {
+	uniqueString := c.Params("uniqueString", "")
+	c.Set("Content-Type", "text/event-stream")
+	c.Set("Cache-Control", "no-cache")
+	c.Set("Connection", "keep-alive")
+	c.Status(fiber.StatusOK).Context().SetBodyStreamWriter(fasthttp.StreamWriter(func(w *bufio.Writer) {
+		ch := mc.addClient(uniqueString)
+		defer mc.removeClient(uniqueString)
+		ticker := time.NewTicker(15 * time.Second)
+		defer ticker.Stop()
+		for {
+			select {
+			case msg := <-ch:
+				fmt.Fprintf(w, "data: %s\n\n", msg)
+				if err := w.Flush(); err != nil {
+					if err == fasthttp.ErrConnectionClosed {
+						return
+					}
+					log.Printf("error while flushing: %v", err)
+					return
+				}
+			case <-ticker.C:
+				fmt.Fprint(w, "heartbeat\n\n")
+				if err := w.Flush(); err != nil {
+					if err == fasthttp.ErrConnectionClosed {
+						return
+					}
+					log.Printf("error while flushing heartbeat: %v", err)
+					return
+				}
+			}
+		}
+	}))
+	return nil
 }
